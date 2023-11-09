ff1573424c16aeceac06c7e31e5960eef468b35f
Isabell Victoria Strandby Ernst <isabell.ernst@hotmail.com>
2023-11-09 10:31:15 +0100

    running targets

--- R/functions.R
+++ R/functions.R
@@ -7,5 +7,5 @@ descriptive_stats <- function(data) {
     data %>%
         dplyr::group_by(metabolite) %>%
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 }
