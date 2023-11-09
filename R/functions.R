50bc3daf2c2f9d32f15179a271f95c098e45ab23
Isabell Victoria Strandby Ernst <isabell.ernst@hotmail.com>
2023-11-09 09:41:19 +0100

    function for descriptive statistics

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' Summary statistics
+#'
+#' @param data, a dataframe
+#'
+#' @return A data.frame/tibble
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
+}
